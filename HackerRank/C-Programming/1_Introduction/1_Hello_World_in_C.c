a30b0c56263a35547657fe8df62cf4ed2cc947ff
Mohamed Ali Haoufa <119907177+MohamedAliHaoufa@users.noreply.github.com>
2023-09-12 10:18:47 +0100

    Update 1_Hello_World_in_C.c

--- HackerRank/C-Programming/1_Introduction/1_Hello_World_in_C.c
+++ HackerRank/C-Programming/1_Introduction/1_Hello_World_in_C.c
@@ -1,17 +1,3 @@
-
-/*
-
- C library Reference site : https://cplusplus.com/reference/cstdio/scanf/
-
- The solutions : https://www.brokenprogrammers.com/playing-with-characters-in-c-hackerrank-solution/
-
-After inputting the character and the string, inputting the sentence by the above mentioned statement won't work.
-This is because, at the end of each line, a new line character \n is present. So, the statement: scanf("%[^\n]%*c", s);
-will not work because the last statement will read a newline character from the previous line. This can be handled in a
-variety of ways and one of them being: scanf("\n"); before the last statement.
-
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
